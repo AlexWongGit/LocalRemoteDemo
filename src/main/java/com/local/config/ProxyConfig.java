0d998cdf9c10dc8cda11fe82271725845bbec22c
wangzf <wangfb@digiwin.com>
2024-11-29 12:53:29 +0800

    refactor(config): 合并配置类并调整组件扫描
    - 删除了 AppConfig.java 文件
    - 将组件扫描配置移至 ProxyConfig.java
    - 更新 ProxyConfig.java，添加了新的包和类的扫描配置
    
    Took 46 seconds

--- src/main/java/com/local/config/ProxyConfig.java
+++ src/main/java/com/local/config/ProxyConfig.java
@@ -3,9 +3,15 @@ package com.local.config;
 import com.test.feign.FeignClientService;
 import com.test.feign.FeignClientService2;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
+@ComponentScan(basePackages = {"com.test","com.becalled"},
+    excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+        value = {com.test.InvokeApplication.class,
+            com.becalled.controller.TTController.class, com.becalled.IsInvokedApplication.class}))
 public class ProxyConfig {
 
     @Bean
