c146681d62454f1d73446ed0763fde4048618f30
wangzf <wangfb@digiwin.com>
2024-11-29 14:32:52 +0800

    refactor(controller): 重命名 TTController 为 TestController
    -将 TTController.java 文件重命名为 TestController.java
    - 更新相关的类引用和配置文件中的名称
    -调整 Controller 类的命名，使其更加清晰和符合命名规范

--- src/main/java/com/local/config/ProxyConfig.java
+++ src/main/java/com/local/config/ProxyConfig.java
@@ -1,6 +1,5 @@
 package com.local.config;
 
-import com.becalled.controller.TestController;
 import com.test.feign.FeignClientService;
 import com.test.feign.FeignClientService2;
 import org.springframework.context.annotation.Bean;
